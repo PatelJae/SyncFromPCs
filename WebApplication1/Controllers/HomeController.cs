8ad0203f5263a7e08e64157a9274746689071c61
Jay Patel <upaj25@gmail.com>
2016-01-08 02:38:13 -0500

    Added Database Files

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -12,5 +12,46 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Table t)
+        {
+            // this action is for handle post login
+            if (ModelState.IsValid)
+            {
+                using (MyTestDatabaseEntities dc = new MyTestDatabaseEntities())
+                {
+                    var v = dc.Tables.Where(a => a.UserName.Equals(t.UserName) && a.Password.Equals(t.Password)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedUserID"] = t.UserID.ToString();
+                        Session["LoggedUserFullName"] = t.FullName.ToString();
+                        return RedirectToAction("AfterLogin");
+                    }
+
+                }
+            }
+
+            return View(t);
+        }
+
+        public ActionResult AfterLogin()
+        {
+            if (Session ["LogedUserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
     }
 }
